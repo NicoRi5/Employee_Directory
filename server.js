24a006a55b5c2435c5aa1c2027153ddad1e09933
Nicole Riera <150852807+NicoRi5@users.noreply.github.com>
2025-01-07 11:42:46 -0500

    First commit

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-// TODO: this file :)
+const express = require("express");
+const app = express(); // was running into issue but had to initialize this before const employees
+const employees = require("./employees");
+
+const PORT = 3000;
+
+// sends a string "Hello employees!"
+app.get("/", (req, res) => {
+  res.send("Hello employees!");
+});
+
+// sends full array of employees
+app.get("/employees", (req, res) => {
+  res.json(employees);
+});
+
+// sends employee with the given id
+app.get("/employees/:id", (req, res) => {
+  const id = req.params.id;
+  const employeeFound = employees.find((e) => e.id === id);
+
+  // sends message if no employee with that id is found
+  if (employees) {
+    res, json(employeeFound);
+  } else {
+    res.status(404).send("No employee with that id.");
+  }
+});
+
+// sends random employee from array
+// *A REQUEST MUST BE HANDLED BY THE *FIRST* HANDLER WITH A MATCHING PATH*
+
+app.get("/employees/random", (req, res) => {
+  const randomEmpIndex = Math.floor(Math.random() * employees.length); // makes sure to check for possible errors regarding array length
+  const randomEmployee = employees[randomEmpIndex];
+  res.json(randomEmployee);
+});
